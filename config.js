afaf86a504b840b0e6ae9a2f753d6ac773fbe87a
khuddusshariff0022@gmail.com <khuddusshariff0022@gmail.com>
2025-11-02 15:26:46 +0530

    Initiam commit
    Signed-off-by: khuddusshariff0022@gmail.com <khuddusshariff0022@gmail.com>

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js - Technician Configuration File
+// Update the details below with your actual business information
+
+const CONFIG = {
+    // Site/Business Name
+    siteName: 'Sufiyan Home Appliance Service',
+    
+    // AC Technician Phone Number
+    acPhone: '+91 9916042908',
+    
+    // Washing Machine Technician Phone Number
+    washingPhone: '+91 9916042908'
+};
